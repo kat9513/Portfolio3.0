6942917f567be7bf4ee8cf660281a6a1d0a42fb6
Katie Higgins <katherine9513@gmail.com>
2019-04-16 23:30:16 -0500

    adding arrow icon for responsive navbar

--- src/components/NavBar/NavBar.component.js
+++ src/components/NavBar/NavBar.component.js
@@ -8,7 +8,9 @@ export default class NavBar extends Component {
                     <a className="navbar-brand" href="/">Katherine Higgins</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon">
+                            <i class="fas fa-arrow-circle-down fa-2x"></i>
+                        </span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav ml-auto">
